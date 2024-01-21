7f6b944e231a55b8e1ab0bf84f9201fb3b7b636d
yongtongyanyi <iwindows7@gmail.com>
2024-01-21 12:20:26 +0800

    Delete season2 directory

--- season2/bbc-iplayer/components/Model.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line import/prefer-default-export
-export interface Channel {
-  id: string;
-  title: string;
-  subtitle: string;
-  type: string;
-  cover: number;
-}
--- season2/safari-tabs/components/Model.ts
+++
@@ -1,8 +0,0 @@
-export const OVERVIEW = -1;
-
-export interface TabModel {
-  id: string;
-  uri: string;
-}
-
-export type TabsModel = TabModel[];
